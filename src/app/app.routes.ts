899f4d8c3401b8aab2890bb692a32afea8a2d758
suzan-pixel <suzanelbadey89@gmail.com>
2025-09-21 03:19:55 +0300

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { ProductComponent } from './component/product/product.component';
 import { NotfoundComponent } from './component/notfound/notfound.component';
+
+
 import { ContactComponent } from './component/contact/contact.component';
 import { AboutComponent } from './component/about/about.component';
 
@@ -14,9 +16,11 @@ export const routes: Routes = [
   { path: 'add-product/:id', component: AddProductComponent },
   { path: 'cart', component: CartComponent },
     {path:'home',component:HomeComponent},
+   
     {path:'product',component:ProductComponent},
         {path:'about',component:AboutComponent},
     {path:'contact',component: ContactComponent},
+    
     {path:'',  redirectTo: 'home', pathMatch:'full'},
  
 
